a83c9dbdd9ea6772c1e8c0c1588523f2b0506d7c
redwan <redtah2007@yahoo.com>
2023-10-13 05:22:44 -0700

    Add exercise

--- test.rb
+++ test.rb
@@ -1,33 +1,11 @@
-> print "Hello World"
-Hello World => nil
+def add_numbers(a, b)
+  result = a + b
+  return result
+end
 
-10/3
-10/3.0
-"Happy" + "coding"
-"Happy" + "\n" +  "coding"
-print "Happy" + "\n" +  "coding" + "\n"
+num1 = 5
+num2 = 3
 
-a = 3
-a
-a = "Hello"
-a
-b = "World!"
-puts(a + b)
+sum = add_numbers(num1, num2)
 
-irb(main)> def hello(name)
-irb(main)>   puts("Hello #{name}!")
-irb(main)> end
-=> :hello
-
-put add(3.14, 1.618)
-    """Returns the addition of the two arguments.
-  
-    Args:
-      a: A number.
-      b: A number.
-  
-    Returns:
-      The sum of a and b.
-    """
-    return a + b
-  end
+puts "The sum is: #{sum}"
