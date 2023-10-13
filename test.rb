58f4970b1291e4c1e02dd6a7ee0fcfeba2e26677
redwan <redtah2007@yahoo.com>
2023-10-13 05:09:47 -0700

    Add diiffernt exercise

---
+++ test.rb
@@ -0,0 +1,33 @@
+> print "Hello World"
+Hello World => nil
+
+10/3
+10/3.0
+"Happy" + "coding"
+"Happy" + "\n" +  "coding"
+print "Happy" + "\n" +  "coding" + "\n"
+
+a = 3
+a
+a = "Hello"
+a
+b = "World!"
+puts(a + b)
+
+irb(main)> def hello(name)
+irb(main)>   puts("Hello #{name}!")
+irb(main)> end
+=> :hello
+
+put add(3.14, 1.618)
+    """Returns the addition of the two arguments.
+  
+    Args:
+      a: A number.
+      b: A number.
+  
+    Returns:
+      The sum of a and b.
+    """
+    return a + b
+  end
